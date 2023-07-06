a1e9066167487ee4bd1ef6f4286080a6ab8a9be3
Khanh Phan Trần <ptrankhanh02@gmail.com>
2023-07-06 22:32:49 +0700

    [feat] server process html and adult content

--- server/index.py
+++ server/index.py
@@ -2,8 +2,39 @@
 Author: vvthai
 Description: Run websocket by python: python index.py
 '''
+import json
 import asyncio
 import websockets
+import transformers
+from bs4 import BeautifulSoup
+from datasets import load_dataset
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, TextClassificationPipeline
+
+base_model_name = "valurank/finetuned-distilbert-adult-content-detection"
+tokenizer = AutoTokenizer.from_pretrained(base_model_name)
+model = transformers.AutoModelForSequenceClassification.from_pretrained(base_model_name, id2label={0: 'safe', 1: 'not safe'})
+pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer, return_all_scores = True)
+
+def is_safe(inpt_text: str):
+    global pipe
+    outp_rate = pipe(inpt_text)
+    return True if max(outp_rate[0], key=lambda x: x['score'])['label'] == 'safe' else False
+
+def extract_adult_pid(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pid = []
+    for p_tag in soup.find_all('p', attrs={'data-p-id': True}):
+        # Check if the <p> tag contains any <span> tags
+        if p_tag.find('span'):
+            # Remove text from nested <span> tags
+            for span_tag in p_tag.find_all('span'):
+                span_tag.extract()
+        # Extract the text from the <p> tag and evaluate it
+        p_text = p_tag.get_text(strip=True).rstrip('\n')
+        # Get list of <p> data-p-id
+        if not is_safe(p_text):
+            pid.append(p_tag.get('data-p-id'))
+    return json.dumps(pid)
 
 async def handle_connection(websocket, path):
     print("New client connected!")
@@ -12,13 +43,13 @@ async def handle_connection(websocket, path):
     try:
         while True:
             message = await websocket.recv()
-            print("Received message from client:", message)
+            print("Received message from client:", message[:10])
 
             # Xử lý dữ liệu nhận được từ client
-
+            json_data = extract_adult_pid(message)
             # Gửi dữ liệu từ server tới client
-            response = "Hello from server!"
-            await websocket.send(response)
+            # response = "Hello from server!"
+            await websocket.send(json_data)
             
     except websockets.exceptions.ConnectionClosedOK:
         print("Client has disconnected")
