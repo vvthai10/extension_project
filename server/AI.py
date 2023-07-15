84469898966c987e05a544505723a7fae5a481b5
ThuanDuongHCMUS <thuanduongmaths@gmail.com>
2023-07-15 18:31:33 +0700

    finish detector
    able to run at real-time runtime
    move code to separate file

---
+++ server/AI.py
@@ -0,0 +1,165 @@
+import transformers
+pipe = transformers.pipeline(model="typeform/distilbert-base-uncased-mnli", task="zero-shot-classification")
+
+def contain_a_z(text: str):
+    # function: check if input text have any char from a to z    
+    for char in text:        
+        int_rep = ord(char.lower())
+        if int_rep in range(97, 123):
+            return True
+    return False
+
+# define a function to standardize text
+def standardize_text(text:str):
+    # only keep characters from a-z and A-Z and space
+    for char in text:
+        if not char.isalpha():
+            text = text.replace(char, '')        
+    return text
+    
+
+# define a function to parse paragraph into sentences
+def parse_paragraph(paragraph:str):          
+    sentences = paragraph.split('.')    
+    # remove sentences without any alphabetic characters
+    for sentence in sentences:
+        if not any(ch.isalpha() for ch in sentence):
+            sentences.remove(sentence)
+            continue
+        for ch in ['  ', '   ', '    ', '     ', '      ']:
+            sentence = sentence.replace(ch, ' ')
+        # remove spaces at the beginning of sentences or end of sentences
+        if sentence[0] == ' ':
+            sentence = sentence[1:]
+        if sentence[-1] == ' ':
+            sentence = sentence[:-1]  
+    return sentences
+
+
+
+def change_text(input: str):
+    # function: change input text to placeholder
+    # para: input: input text
+    # return: output: placeholder text (same length with input text, only contain 'X')
+    output = ''
+    for char in input:
+        output += 'X'
+    return output
+
+def match_label(text: str, target_label: str, threshold = 0.5):
+    global pipe        
+    output = pipe(text, target_label, multi_label=False)            
+    if output['scores'][0] >= threshold:        
+        return True
+    return False
+
+def init_queue(text_list:list, bucket_len = 512):    
+    # para: text_list: list of text in each tag
+    #      bucket_len: maximum length of concatenated text, larger num = faster runtime  (default = 512)
+    # return queue: queue of index range that have not been checked        
+    queue = [] 
+    n = len(text_list)
+    index = 0
+    while index < n:
+        current_text_len = 0
+        low = index
+        # accumulate text until length >= 512
+        while index < n:
+            current_text_len += len(text_list[index])
+            if index != n-1 and current_text_len + len(text_list[index+1]) < bucket_len:
+                index += 1        
+            else:
+                break
+        high = index                        
+        queue.append((low, high))
+        index += 1
+    return queue
+
+def clear_non_label(range_list: list, text_list:list, label: str):
+    # function: delete bucket that does not match with label         
+    # para: range_list: list of range to check        
+    # return cleared_list: queue of index range that match with label                
+    for item in range_list:
+        # recreate text in range
+        current_text = ''
+        for index in range(item[0], item[1] + 1):
+            current_text += text_list[index]
+        # check if current_text match with label
+        if not match_label(current_text, label):
+            # delete range from queue
+            range_list.remove(item) 
+    return range_list    
+
+def find_label_index(range_queue: list, text_list:list, label: str):
+    # function: find index of tag match with label using binary search
+    # para: range_queue: list of range to check
+    #       text_list: list of text in each tag
+    # return matched_text: list of range match label (precision down to one tag)
+    matched_text = []
+    while len(range_queue) > 0:
+        low, high = range_queue.pop(0)    
+        if low > high:
+            continue
+        elif low == high: # range only contain a single tag
+            matched_text.append((low, high))
+        else:        
+            mid = (low + high) // 2
+            # handle each half                
+            # recreate text in range
+            current_text = ''
+            for index in range(low, mid + 1):
+                current_text += text_list[index]
+            # check further if range match with label
+            if match_label(current_text, label):
+                range_queue.append((low, mid))
+            # do like so with the second half            
+            current_text = '' 
+            for index in range(mid + 1, high + 1):
+                current_text += text_list[index]
+            if match_label(current_text, label):
+                range_queue.append((mid +1, high))
+    return matched_text
+
+def find_content_id(tag_text: list, id:list, label: str):    
+    res_text = [] # list of text that match with label (maybe edited)
+    res_id = [] # list of id that correspond to res_text 
+# standardize input text
+    # for index in range(len(tag_text)):
+    #     tag_text[index] = standardize_text(tag_text[index])
+# find label on tag level
+    # initialize queue of index range to be checked      
+    queue = init_queue(tag_text, 200)
+    # clear range that does not match with label
+    queue = clear_non_label(range_list= queue, text_list= tag_text, label= label)
+    # find index of tag match with label using binary search
+    tag_index = find_label_index(range_queue= queue, text_list= tag_text, label= label)            
+# find label on sentence level
+    index_dict = {} # index of tag_text and tag_id in input that contain each sentence
+    # break down each tag into sentences and bind the sentences to tag's id
+    sentence_text = []
+    for tag in tag_index:
+        index , high = tag        
+        # parse paragraph into sentences    
+        sentences = parse_paragraph(tag_text[index])        
+        # create a dictionary of sentence to id
+        for sentence in sentences:
+            index_dict[sentence] = index
+            sentence_text.append(sentence)
+# find label in sentence level by doing the same thing with paragraph level
+    queue = init_queue(text_list=sentence_text, bucket_len=50)
+    queue = clear_non_label(range_list= queue, text_list= sentence_text, label= label)
+    sentence_index = find_label_index(range_queue= queue, text_list= sentence_text, label= label)
+    # create new sentence from ones matched with label
+    for sentence in sentence_index:
+        index, high = sentence
+        # get index of tag that contain current sentence
+        base_index = index_dict[sentence_text[index]]
+        # get id of tag that contain current sentence
+        old_id = id[base_index]                     
+        # replace the sentence in tag with placeholder
+        old_text = tag_text[base_index]        
+        new_text = old_text.replace(tag_text[base_index], change_text(sentence_text[index]))        
+        # add new_text and id to result
+        res_id.append(old_id)
+        res_text.append(new_text)
+    return res_text, res_id
--- server/index.py
+++ server/index.py
@@ -5,95 +5,32 @@ Description: Run websocket by python: python index.py
 import json
 import asyncio
 import websockets
-import transformers
 from bs4 import BeautifulSoup
-import pickle as pkl
+from AI import find_content_id, contain_a_z
 
-
-basic_labels = ['romantic love' ,'daily talk', 'complaint', 'sarcasm', 'humor', 'anger', 'sadness', 'joy', 'fear', 'surprise', 'disgust', 'curiosity', 'other']
-pipe = transformers.pipeline("zero-shot-classification", model="facebook/bart-large-mnli", topk=None)
-
-
-# define a function to standardize text
-def standardize_text(text:str):
-    # only keep characters from a-z and A-Z and space
-    for char in text:
-        if not char.isalpha():
-            text = text.replace(char, '')        
-    return text
-    
-
-# define a function to parse paragraph into sentences
-def parse_paragraph(paragraph:str):          
-    paragraph = standardize_text(paragraph)  
-    sentences = paragraph.split('.')    
-    # remove sentences without any alphabetic characters
-    for index in range(len(sentences)):        
-        if not any(ch.isalpha() for ch in sentences[index]):
-            sentences.pop(index)
-            continue
-        # remove abundant spaces
-        for ch in ['  ', '   ', '    ', '     ', '      ']:
-            sentences[index] = sentences[index].replace(ch, ' ')
-        # remove spaces at the beginning of sentences or end of sentences
-        if sentences[index][0] == ' ':
-            sentences[index] = sentences[index][1:]
-        if sentences[index][-1] == ' ':
-            sentences[index] = sentences[index][:-1]
-    return sentences
-
-def match_label(inpt_text: str, target_label = "sexual"):
-    # check if input text have any char from a to z
-    def contain_a_z(text: str):
-        for char in text:        
-            int_rep = ord(char.lower())
-            if int_rep in range(97, 123):
-                return True
-        return False
-    
-    #handle input text
-    if not contain_a_z(inpt_text):
-        return False
-    global pipe, basic_labels   
-    # create a list of labels (target label + basic labels), more labels is better for classification    
-    labels = basic_labels.copy()
-    if target_label not in labels:
-        labels.append(target_label)    
-    # parse and standardize paragraph into sentences
-    sentences = parse_paragraph(inpt_text)
-    # check if any sentence match with target label 
-    # p/s: will use divide and conquer algorithm to reduce time complexity in the future
-    for sentence in sentences:
-        output = pipe(sentence, labels, multi_label=True)        
-        if output['labels'][0] == target_label:
-            return True
-    return False
+            
 
 def extract_adult_pid(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    pid = []
-    show_progress = True # show progress of extracting and predicting every 5 tags
-    index = 1
+    soup = BeautifulSoup(html, 'html.parser')    
     tag_list = soup.find_all('p', attrs={'data-p-id': True})
-    for p_tag in tag_list:
-        if show_progress and index % 5 == 0:
-            print('Extracting tag no.' + str(index) + 'out of '+ str(len(tag_list)) + '...')
+    text_list = []
+    id_list = []
+    for p_tag in tag_list:        
         # Check if the <p> tag contains any <span> tags
         if p_tag.find('span'):
             # Remove text from nested <span> tags
             for span_tag in p_tag.find_all('span'):
                 span_tag.extract()
         # Extract the text from the <p> tag and evaluate it
-        p_text = p_tag.get_text(strip=True).rstrip('\n')           
-        if show_progress and index % 5 == 0:
-            print('Predicting tag no.' + str(index) + 'out of '+ str(len(tag_list)) + '...')     
-        # Get list of <p> data-p-id
-        if match_label(p_text, 'sexual'):
-            pid.append(p_tag.get('data-p-id'))            
-        if show_progress and index % 5 == 0:
-            print('Finish tag no.' + str(index) + 'out of '+ str(len(tag_list)) + '...')
-        index += 1
-    return json.dumps(pid)
+        p_text = p_tag.get_text(strip=True).rstrip('\n')
+        # check if p_text have any char from a to z
+        if not contain_a_z(p_text):
+            continue
+        text_list.append(p_text)
+        id_list.append(p_tag.get('data-p-id'))
+    new_tag_text, tag_id = find_content_id(text_list, id_list, 'sexual')   
+    # TODO: send new_text, id to server
+    return json.dumps((tag_id, new_tag_text))
 
 async def handle_connection(websocket, path):
     print("New client connected!")
@@ -115,11 +52,14 @@ async def handle_connection(websocket, path):
                 "tabId": data['tabId'],
                 "message": json_data
             }
+            # DEBUG
+            print('Send data to client: ', data)
             await websocket.send(json.dumps(data))
             
     except websockets.exceptions.ConnectionClosedOK:
         print("Client has disconnected")
-
+#DEBUG 
+print('ready to connect to client')
 start_server = websockets.serve(handle_connection, "localhost", 8082)
 
 asyncio.get_event_loop().run_until_complete(start_server)
